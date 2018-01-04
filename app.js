b19988a3cedc5aa5564641804681006fbc85bcbe
Kevin Gutierrez <kevingutierrezg@gmail.com>
2018-01-04 14:55:32 -0500

    Add nodemon

--- app.js
+++ app.js
@@ -5,58 +5,67 @@ const rp = require('request-promise');
 const Excel = require('exceljs');
 const convert = require('xml-js');
 
-const URL = 'http://172.16.1.33:7800/esb/service/SedeElectronica/';
+const URLDEV = 'http://172.16.1.33:7800/esb/service/SedeElectronica/';
+const URLPROD = 'http://172.16.1.127:7800/esb/service/SedeElectronica/';
 
 const xlsxFile = `${__dirname}/documents/RECUPERA_MPN_VIRTUAL_2017-12-29.xlsx`;
 const workbook = new Excel.Workbook();
 
-const testXml = `
-    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ws="http://ws.recaudos.esb.ccb.org.co">
-        <soapenv:Header/>
-        <soapenv:Body>
-            <ws:registrarFormularios>
-                <metadata/>
-                <data>
-                    <registrarFormulariosInDTO>
-                    <numSolicitud>578031</numSolicitud>
-                    <numOrdenPago>0010382079</numOrdenPago>
-                    <numTramite>000001700000611</numTramite>
-                    <idTipoFormulario>1</idTipoFormulario>
-                    <idTipoModelo>2</idTipoModelo>
-                    <idTipoAplicativo>1</idTipoAplicativo>
-                    </registrarFormulariosInDTO>
-                </data>
-            </ws:registrarFormularios>
-        </soapenv:Body>
-    </soapenv:Envelope>`;
+// const testXml = `
+//     <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ws="http://ws.recaudos.esb.ccb.org.co">
+//         <soapenv:Header/>
+//         <soapenv:Body>
+//             <ws:registrarFormularios>
+//                 <metadata/>
+//                 <data>
+//                     <registrarFormulariosInDTO>
+//                     <numSolicitud>578031</numSolicitud>
+//                     <numOrdenPago>0010382079</numOrdenPago>
+//                     <numTramite>000001700000611</numTramite>
+//                     <idTipoFormulario>1</idTipoFormulario>
+//                     <idTipoModelo>2</idTipoModelo>
+//                     <idTipoAplicativo>1</idTipoAplicativo>
+//                     </registrarFormulariosInDTO>
+//                 </data>
+//             </ws:registrarFormularios>
+//         </soapenv:Body>
+//     </soapenv:Envelope>`;
 
 // doRequest(URL, testXml);
 
 const promises = [];
 const rows = [];
 
+let hasChanged = false;
+
 workbook.xlsx.readFile(xlsxFile)
     .then(() => {
         const worksheet = workbook.getWorksheet('RECUPERA');
         const rowCount = worksheet.rowCount;
 
-        console.log('Row Count', rowCount);
+        // Total 1287
+        const initial = 1;
+        const count = 10;
 
-        for (let i = 2; i < 42; i++) {
+        console.log('Current count', count);
+        console.log('Row count', rowCount);
+
+        for (let i = initial; i < count; i++) {
 
             const row = worksheet.getRow(i);
 
-            const index = row.getCell(1).value;
-            const numSolicitud = row.getCell(3).value;
-            const numOrdenPago = row.getCell(5).value;
-            const numTramite = row.getCell(7).value;
-            const currentHash = row.getCell(10).value;
+            const index = row.getCell('A').value;
+            const numSolicitud = row.getCell('C').value;
+            const numOrdenPago = row.getCell('E').value;
+            const numTramite = row.getCell('G').value;
+            const currentHash = row.getCell('J').value;
 
-            // Validar que el index sea mayor a 0, 
+            // Validar que el index sea mayor o igual a 0, 
             // exista un numero de solicitud, 
             // un numero de orden de pago, 
             // un numero de tramite y que no se haya generado un hash
-            if (index > 0 && numSolicitud && numOrdenPago && numTramite && currentHash == null) {
+            if (index >= 0 && numSolicitud > 0 && numOrdenPago && numTramite && currentHash == null) {
+                hasChanged = true;
                 console.log('index:', index);
                 console.log('numSolicitud:', numSolicitud);
                 console.log('numOrdenPago:', numOrdenPago);
@@ -84,22 +93,27 @@ workbook.xlsx.readFile(xlsxFile)
                     </soapenv:Envelope>
                 `;
 
-                promises.push(doRequest(URL, xml));
+                promises.push(doRequest(URLPROD, xml));
                 rows.push(row);
             }
         }
 
-        promises.reduce((promiseChain, currentTask) => {
-            return promiseChain.then(chainResults =>
-                currentTask.then(currentResult => [...chainResults, currentResult])
+        const resultPromise = promises.reduce((promise, currentPromise, index) => {
+            return promise.then(_ =>
+                currentPromise.then(result => modifyExcel(result, rows[index])).catch()
             );
-        }, Promise.resolve([])).then(arrayOfResults => {
-            arrayOfResults.forEach((result, index) => modifyExcel(result, rows[index]));
-            workbook.xlsx.writeFile(xlsxFile)
-                .then(() => {
-                    console.log('Done');
-                }).catch(onError);
-        }).catch(onError);
+        }, Promise.resolve());
+
+        resultPromise.then(_ => {
+            console.log('All promises where executed');
+            if (hasChanged) {
+                workbook.xlsx.writeFile(xlsxFile)
+                    .then(() => {
+                        console.log('The file was modified successfully');
+                    }).catch(onError);
+            }
+        });
+
     }).catch(onError);
 
 function doRequest(uri, body) {
@@ -119,7 +133,8 @@ function doRequest(uri, body) {
 }
 
 function modifyExcel(parsedBody, row) {
-    console.log(new Date());
+    const index = row.getCell('A').value;
+    console.log(`${index}: ${new Date()}`);
 
     const jsonString = convert.xml2json(parsedBody, {
         compact: true
@@ -135,8 +150,6 @@ function modifyExcel(parsedBody, row) {
     let hasErrors = true;
     let hashText;
 
-    const index = row.getCell(1).value;
-
     try {
         hasErrors = json[firstLevel][secondLevel][thirdLevel].data.registrarFormulariosOutDTO.resultado.codigoError._text !== '0000';
         hashText = json[firstLevel][secondLevel][thirdLevel].metadata.transactionID._text;
@@ -146,7 +159,7 @@ function modifyExcel(parsedBody, row) {
 
     if (hasErrors === false && hashText) {
         console.log(`The hash for index ${index} is: ${hashText}`);
-        row.getCell(10).value = hashText;
+        row.getCell('J').value = hashText;
     } else if (hasErrors) {
         console.error('Couldn\'t generate the hash, it was an error in the execution');
     } else if (!hashText) {
@@ -157,3 +170,7 @@ function modifyExcel(parsedBody, row) {
 function onError(error) {
     console.error(error);
 }
+
+// function sleep() {
+//     return new Promise(resolve => setTimeout(() => console.log('llego'), 1000));
+// }
