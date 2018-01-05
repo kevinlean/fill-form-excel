d6d4a412f51be638f4598abc30c5361381c14db6
Kevin Gutierrez <kevingutierrezg@gmail.com>
2018-01-05 16:56:06 -0500

    Add constants

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
+/* jshint esversion: 6 */
 const http = require('http');
 
-// const request = require('sync-request');
 const rp = require('request-promise');
 const Excel = require('exceljs');
 const convert = require('xml-js');
 
-const URLDEV = 'http://172.16.1.33:7800/esb/service/SedeElectronica/';
-const URLPROD = 'http://172.16.1.127:7800/esb/service/SedeElectronica/';
+const URL_DEV = 'http://172.16.1.33:7800/esb/service/SedeElectronica/';
+const URL_PROD = 'http://172.16.1.127:7800/esb/service/SedeElectronica/';
 
 const xlsxFile = `${__dirname}/documents/SinDireccion_2.xlsx`;
 const workbook = new Excel.Workbook();
 
-// const testXml = `
+// const TEST_XML = `
 //     <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ws="http://ws.recaudos.esb.ccb.org.co">
 //         <soapenv:Header/>
 //         <soapenv:Body>
@@ -31,10 +31,16 @@ const workbook = new Excel.Workbook();
 //         </soapenv:Body>
 //     </soapenv:Envelope>`;
 
-// doRequest(URL, testXml);
+// doRequest(URL, TEST_XML);
 
-const promises = [];
-const rows = [];
+const INDEX_CELL = 'A';
+const NUM_SOLICITUD_CELL = 'B';
+const NUM_ORDEN_CELL = 'C';
+const NUM_TRAMITE_CELL = 'D';
+const HASH_CELL = 'G';
+
+let promises = [];
+let rows = [];
 
 let hasChanged = false;
 
@@ -43,7 +49,7 @@ workbook.xlsx.readFile(xlsxFile)
         const worksheet = workbook.getWorksheet('Sheet1');
         const rowCount = worksheet.rowCount;
 
-        // Total 692
+        // Total: 692
         const initial = 1;
         const limit = 10;
 
@@ -54,15 +60,15 @@ workbook.xlsx.readFile(xlsxFile)
 
             const row = worksheet.getRow(i);
 
-            const index = row.getCell('A').value;
-            const numSolicitud = row.getCell('B').value;
-            const numOrdenPago = row.getCell('C').value;
-            const numTramite = row.getCell('D').value;
-            const currentHash = row.getCell('G').value;
+            const index = row.getCell(INDEX_CELL).value;
+            const numSolicitud = row.getCell(NUM_SOLICITUD_CELL).value;
+            const numOrdenPago = row.getCell(NUM_ORDEN_CELL).value;
+            const numTramite = row.getCell(NUM_TRAMITE_CELL).value;
+            const currentHash = row.getCell(HASH_CELL).value;
 
             // Validar que el index sea mayor o igual a 0,
             // exista un numero de orden de pago,
-            // un numero de tramite y que no se haya generado un hash
+            // un numero de tramite y que no se haya generado un hash.
             if (index >= 0 && index !== null && numOrdenPago && numTramite && currentHash == null) {
                 hasChanged = true;
                 console.log('index:', index);
@@ -92,14 +98,17 @@ workbook.xlsx.readFile(xlsxFile)
                     </soapenv:Envelope>
                 `;
 
-                promises.push(doRequest(URLPROD, xml));
+                promises.push(doRequest(URL_DEV, xml));
                 rows.push(row);
             }
         }
 
         const resultPromise = promises.reduce((promise, currentPromise, index) => {
             return promise.then(_ =>
-                currentPromise.then(result => modifyExcel(result, rows[index])).catch()
+                currentPromise.then(result => {
+                    modifyExcel(result, rows[index]);
+                    console.log(`Executed promise with index ${index}`);
+                }).catch()
             );
         }, Promise.resolve());
 
@@ -132,7 +141,7 @@ function doRequest(uri, body) {
 }
 
 function modifyExcel(parsedBody, row) {
-    const index = row.getCell('A').value;
+    const index = row.getCell(INDEX_CELL).value;
     console.log(`${index}: ${new Date()}`);
 
     const jsonString = convert.xml2json(parsedBody, {
@@ -158,7 +167,7 @@ function modifyExcel(parsedBody, row) {
 
     if (hasErrors === false && hashText) {
         console.log(`The hash for index ${index} is: ${hashText}`);
-        row.getCell('G').value = hashText;
+        row.getCell(HASH_CELL).value = hashText;
     } else if (hasErrors) {
         console.error('Couldn\'t generate the hash, it was an error in the execution');
     } else if (!hashText) {
